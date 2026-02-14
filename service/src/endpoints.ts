f8dd110c1e4f75e53405761f427c6de924500428
Tobias Herber <22559657+herber@users.noreply.github.com>
2026-02-14 17:45:58 +0100

    Add better health check

--- service/src/endpoints.ts
+++ service/src/endpoints.ts
@@ -1,4 +1,4 @@
-import { redis } from 'bun';
+import { RedisClient } from 'bun';
 import { RelayApi } from './controllers';
 import { db } from './db';
 
@@ -12,10 +12,16 @@ console.log(`Service running on http://localhost:${server.port}`);
 Bun.serve({
   fetch: async _ => {
     try {
-      await db.sender.count();
+      await db.emailIdentity.count();
+
+      let redis = new RedisClient(process.env.REDIS_URL?.replace('rediss://', 'redis://'), {
+        tls: process.env.REDIS_URL?.startsWith('rediss://')
+      });
       await redis.ping();
+
       return new Response('OK');
     } catch (e) {
+      console.error('Health check failed', e);
       return new Response('Service Unavailable', { status: 503 });
     }
   },
